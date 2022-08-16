745366248ffad1283e80827231fe4a678548f4a7
Mohammed Naji <m.naji1512@gmail.com>
2022-08-16 16:47:24 +0300

    lec13

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Post;
+use App\Models\Tag;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -15,6 +17,10 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        Post::truncate();
+        Tag::truncate();
+        Post::factory(100)->create();
+        Tag::factory(100)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
